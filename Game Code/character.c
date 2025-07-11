f8a82ef65c7ceecfd63502236aa3493c53242704
Vicente0670 <business0670@gmail.com>
2025-07-10 19:39:34 -0500

    Vacuuming the coding floors
    Co-authored-by: Angelo5181 <Angelo5181@users.noreply.github.com>

--- Game Code/character.c
+++ Game Code/character.c
@@ -1,14 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-struct character {
+struct character { // create a character, char name[22], int maxhp, int hp, int maxmp, int mp, int atk, bool isplayer
   char name[22];
   signed int maxhp;
   signed int hp;
   signed int maxmp;
   signed int mp;
   signed int atk;
-  bool player;
+  bool isplayer;
 };
 
 void printCharacter(struct character x) {
@@ -17,17 +17,12 @@ void printCharacter(struct character x) {
 
   printf("Name: %s\n", x.name);
   printf("HP: %d/%d\n", x.hp,x.maxhp);
-  printf("MP: %d\n", x.mp);
+  printf("MP: %d/%d\n", x.mp,x.maxmp);
   printf("ATK: %d\n\n", x.atk);
 }
 
 bool isDead(struct character x){
-  //printf("x.hp:%d\n",x.hp);
-  if(x.hp<=0){
-    //printf("%s is dead.\n",x.name);
-    return true;
-  }
-  //printf("%s is not dead.\n",x.name);
+  if (x.hp <= 0) return true;
   return false;
 }
 
@@ -35,4 +30,9 @@ int findVictor(struct character p, struct character e){
   if (isDead(p)) return 1;
   if (isDead(e)) return 2;
   return 0;
+}
+
+void limitStats(struct character x) {
+  if (x.hp > x.maxhp) x.hp = x.maxhp;
+  if (x.mp > x.maxmp) x.mp = x.maxmp;
 }
